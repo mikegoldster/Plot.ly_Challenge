aca727686f186c5142e85c15ef063489179604ea
Mikegoldster <mikegoldster@gmail.com>
2019-05-31 16:47:24 -0500

    Larging Working Project

--- static/js/app.js
+++ static/js/app.js
@@ -2,56 +2,66 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
   // Use `d3.json` to fetch the metadata for a sample
   var MetaData = `/metadata/${sample}`;
-  d3.json(MetaData).then(function(data) {
-  // Use d3 to select the panel with id of `#sample-metadata`
-    var bbSample = d3.select("#sample-metada");
-  // Use `.html("") to clear any existing metadata
+  d3.json(MetaData).then(function(bbData) {
+    // Use d3 to select the panel with id of `#sample-metadata` 
+    var bbSample = d3.select("#sample-metadata");
+    // Use `.html("") to clear any existing metadata
     bbSample.html("");
-  // Use `Object.entries` to add each key and value pair to the panel
-  //   Object.entries(data).forEach(([key, value]) => {
-  //     bbSample.append("p")
-  //     .text(`${key}:${value}`);
-  // });
-    //
-    Object.entries(data).forEach(function([key, value]) {
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+     // tags for each key-value in the metadata.
+    Object.entries(bbData).forEach(function([key, value]) {
       console.log(key, value)
-      var rowSample = (`${key}: ${value}`);
-        var cell = bbSample.append("h5");
-        cell.text(rowSample);
+      var row = bbSample.append("p");
+      row.text(`${key}: ${value}`);
     });
   });
 }
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
+   
 
 function buildCharts(sample) {
-}
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-
-
-
+  var plotData = `/samples/${sample}`;
     // @TODO: Build a Bubble Chart using the sample data
+    d3.json(plotData).then(function(bubbleData) {
+      var bubbleX = bubbleData.otu_ids;
+      var bubbleY = bubbleData.sample_values;
+      var markerSize = bubbleData.sample_values;
+      var markerColor = bubbleData.otu_ids;
+      var textValue = bubbleData.otu_labels;
+      
+      var trace1 = {
+        x: bubbleX,
+        y: bubbleY,
+        text: textValue,
+        mode: 'markers',
+        marker: {
+          color: markerColor,
+          size: markerSize,
+        }
+      };
 
+      var bubbleTrace = [trace1];
 
-
+      Plotly.newPlot('bubble', bubbleTrace);
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+  d3.json(plotData).then(function(pieData)  {
+    var pieValues = pieData.sample_values.slice(0,10)
+    var pieLabels = pieData.otu_ids.slice(0,10)
+    var pieHover = pieData.otu_labels.slice(0,10)
+    var pieCreate = [{
+      type: "pie",
+      values: pieValues,
+      labels: pieLabels,
+      hoverinfo: pieHover,
+      }];
 
-//     var bbpie = {
-//   labels: [],
-//   values: [],
-//   type: 'pie'
-// };
-//
-// var data = [bbpie];
-//
-// var layout = {
-//   title: "Belly Button Chart",
-// };
-//
-// Plotly.newPlot("plot", data, layout);
-// }
+    Plotly.newPlot("pie", pieCreate);
+    });
+  });
+};
 
 function init() {
   // Grab a reference to the dropdown select element
